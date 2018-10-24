062f11e209594c7ced250f168ad104a4ae24f5ca
David Konsumer <konsumer@jetboystudio.com>
2018-10-23 17:03:58 -0700

    Update ba-engine.tsx

--- src/components/ba-engine/ba-engine.tsx
+++ src/components/ba-engine/ba-engine.tsx
@@ -13,7 +13,7 @@ export class BaEngine {
   }
 
   render() {
-    const out = [<canvas ref={el => this.canvas = el}></canvas>]
+    const out = [<canvas ref={el => this.canvas = this.canvas || el}></canvas>]
     if (this.engine){
       out.push(
         <Tunnel.Provider state={{engine: this.engine}}>
